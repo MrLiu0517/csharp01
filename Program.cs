a1381ea1a45008659259efd9bc559738dbae0a0a
MrLiu0517 <liuyin70517@163.com>
2023-03-29 14:05:21 +0800

    泛型集合

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
-﻿namespace 基础01;
+﻿namespace 泛型集合;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        string str = @"/Users/liuyin/GitHub/csharp01/Program.cs";
-        //获取文件名
-        Console.WriteLine(Path.GetFileName(str));
-        //获取文件名，但不包含扩展名
-        Console.WriteLine(Path.GetFileNameWithoutExtension(str));
-        //获取文件的扩展名
-        Console.WriteLine(Path.GetExtension(str));
-        //获取文件所在文件夹的路径
-        Console.WriteLine(Path.GetDirectoryName(str));
-        //获取文件所在的全路径
-        Console.WriteLine(Path.GetFullPath(str));
-
-        //创建文件
-        //File.Create(@"/Users/liuyin/GitHub/csharp01/ceshi.txt");
-        //删除一个文件
-        File.Delete(@"/Users/liuyin/GitHub/csharp01/ceshi.txt");
+        //创建泛型集合对象
+        List<int> list = new List<int> { };
+        //添加单个元素
+        list.Add(1);
+        list.Add(2);
+        //添加数组
+        list.AddRange(new int[] { 1, 2, 3, 4, 3, 2, 12, 4 });
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
     }
 }
+
