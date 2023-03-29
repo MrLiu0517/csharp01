c6b0dc6fe74c04812b73bdd0c78c9a5d89967806
MrLiu0517 <liuyin70517@163.com>
2023-03-29 13:33:50 +0800

    路径类

--- Program.cs
+++ Program.cs
@@ -4,5 +4,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        string str = @"/Users/liuyin/GitHub/csharp01/Program.cs";
+        //获取文件名
+        Console.WriteLine(Path.GetFileName(str));
+        //获取文件名，但不包含扩展名
+        Console.WriteLine(Path.GetFileNameWithoutExtension(str));
+        //获取文件的扩展名
+        Console.WriteLine(Path.GetExtension(str));
+        //获取文件所在文件夹的路径
+        Console.WriteLine(Path.GetDirectoryName(str));
+        //获取文件所在的全路径
+        Console.WriteLine(Path.GetFullPath(str));
+
+        //创建文件
+        //File.Create(@"/Users/liuyin/GitHub/csharp01/ceshi.txt");
+        //删除一个文件
+        File.Delete(@"/Users/liuyin/GitHub/csharp01/ceshi.txt");
     }
 }
