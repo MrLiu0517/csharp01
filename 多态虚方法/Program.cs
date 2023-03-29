35367f3bf45216296794509a2f5dca728e9fc18a
MrLiu0517 <liuyin70517@163.com>
2023-03-29 19:20:57 +0800

    虚方法

---
+++ 多态虚方法/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using 多态虚方法;
+
+FuBao fuBao = new FuBao("大福宝");
+DCM dCM = new DCM("大聪明");
+SB sB = new SB("三宝");
+Cat[] cats = { fuBao, dCM, sB };
+for (int i = 0; i < cats.Length; i++)
+{
+    cats[i].SayHi();
+}
+namespace 多态虚方法
+{
+    class Cat
+    {
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public Cat(string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void SayHi()
+        {
+
+        }
+    }
+
+    class FuBao : Cat
+    {
+        public FuBao(string name) : base(name)
+        {
+        }
+        public override void SayHi()
+        {
+            Console.WriteLine("大家好! 我叫{0}", this.Name);
+        }
+
+    }
+
+    class DCM : Cat
+    {
+        public DCM(string name) : base(name)
+        {
+        }
+        public override void SayHi()
+        {
+            Console.WriteLine("大家好! 我叫{0}", this.Name);
+        }
+
+    }
+
+    class SB : Cat
+    {
+        public SB(string name) : base(name)
+        {
+        }
+        public override void SayHi()
+        {
+            Console.WriteLine("大家好! 我叫{0}", this.Name);
+        }
+
+    }
+}
